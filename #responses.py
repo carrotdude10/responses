a4aca31eb7f3790772f855a1bac93beccde72cb7
carrotdude10 <104916793+carrotdude10@users.noreply.github.com>
2022-05-06 10:35:28 -0400

    Add files via upload

---
+++ #responses.py
@@ -0,0 +1,11 @@
+#responses
+
+if total_score > 0 and < 5
+print("You are healthy mentally, but if you ever feel down or like you need some extra support you can try to exercise, eat well, or make some time for your hobbies.")
+
+if total_score > 5 and < 10
+print("It's okay to be in distress sometimes, but make sure you stay healthy mentally. Try to exercise, eat well, and make some time for your hobbies to better your wellbeing.")
+
+if total_score > 10 and < 15
+print("Seek some help (866-392-8523). You are in distress and it's effecting your wellbeing. Try to exercise, eat well, and make some time for your hobbies.")
+
